45f1a5414fa561789ab30556d6f280c0e83fc420
oussamawho <osmwho@gmail.com>
2023-08-16 06:18:49 -0700

    x

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,11 @@
 
 int main(void)
 {
-    char ch[] = "_putchar";
-    int i;
-    for(i = 0; i < 8; i++)
-    {
-        putchar(ch[i]);
-
-    }
-    return 0;
+char ch[] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+{
+putchar(ch[i]);
+}
+return (0);
 }
