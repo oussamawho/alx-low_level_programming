a64ab8a59d0cc386622e893d98a1e4e2b3cdc2cd
oussamawho <osmwho@gmail.com>
2023-08-16 07:53:18 -0700

    x

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
     if (n > 0)
     {
-        _putchar('+');
+        _putchar('43');
         return (1);
     }
     else if (n == 0)
     {
-        _putchar('0'); // Use character '0' instead of 48
+        _putchar('48'); // Use character '0' instead of 48
         return (0);
     }
-    else if (n < 0)
+    else if
     {
-        _putchar('-');
+        _putchar('45');
         return (-1);
     }
 }
