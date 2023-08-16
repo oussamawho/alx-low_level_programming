ea4114ad937decf3771f5c5f85af0cce54a90845
oussamawho <osmwho@gmail.com>
2023-08-16 06:11:37 -0700

    x

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-i#include "main.h"
+#include "main.h"
 /**
  * main - print _putchar
  * is written by mbah
@@ -7,14 +7,12 @@ i#include "main.h"
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+    char ch[] = "_putchar";
+    int i;
+    for(i = 0; i < 8; i++)
+    {
+        putchar(ch[i]);
+
+    }
+    return 0;
 }
