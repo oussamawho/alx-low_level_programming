cb0cd320743995cf5f89584cb3ea37158635d482
oussamawho <osmwho@gmail.com>
2023-08-18 16:25:52 -0700

    x

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * print_numbers - print 0-9
+ * prints the numbers, from 0 to 9, followed by a new line
+ * return: void
+ */
+
+void print_numbers(void)
+{
+int c;
+for (c = 48; c <= 57; c++)
+{
+_putchar (c);
+}
+_putchar ('\n');
+}
