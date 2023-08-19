3a8d0c4d27e17adeb53c21df47a3975a04260c03
oussamawho <osmwho@gmail.com>
2023-08-18 17:28:32 -0700

    x

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,23 +1,30 @@
 #include "main.h"
 
 /**
- * main - prints the numbers from 1 to 100, followed by a new line
- * but for multiples of three prints Fizz instead of the number
- * and for the multiples of five prints Buzz
- * Return: Always 0 (Success)
+ * Prints a triangle made of '#' characters followed by a newline.
+ * The triangle's size is determined by the input parameter.
+ * If the size is 0 or negative, only a newline is printed.
+ *
+ * @param size The size of the triangle to be printed.
+ *             If size is 0 or less, only a newline is printed.
  */
 
-void print_triangle(int size) {
-if (size <= 0) {
+void print_triangle(int size)
+{
+if (size <= 0)
+{
 _putchar('\n');
 return;
 }
 
-for (int row = 1; row <= size; row++) {
-for (int space = 0; space < size - row; space++) {
+for (int row = 1; row <= size; row++)
+{
+for (int space = 0; space < size - row; space++)
+{
 _putchar(' ');
 }
-for (int hash = 0; hash < row; hash++) {
+for (int hash = 0; hash < row; hash++)
+{
 _putchar('#');
 }
 _putchar('\n');
