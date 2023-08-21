c420f18dc361acc28061cd53f187a082c8182f70
oussamawho <osmwho@gmail.com>
2023-08-21 16:16:27 -0700

    x

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+int e = 0;
+while (s[e])
+e++;
+while
+{
+e--;
+_putchar(s[e]);
+}
+_putchar('\n');
+}
