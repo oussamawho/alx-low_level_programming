8fd650aa3199c652587d455bf60f7b5e27600d60
oussamawho <osmwho@gmail.com>
2023-08-21 15:56:56 -0700

    x

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,13 +3,11 @@
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
  */
-
 void _puts(char *str)
 {
-	 while (*str != '\0')
+while (*str != '\0')
 {
-_putchar(*str + 0);
-str++;
+_putchar(*str++);
 }
 _putchar('\n');
 }
