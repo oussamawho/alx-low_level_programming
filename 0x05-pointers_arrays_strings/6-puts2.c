22ddb7e7026a3114e428b178a0f783920248ddab
oussamawho <osmwho@gmail.com>
2023-08-21 17:04:37 -0700

    x

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,10 @@
 void puts2(char *str)
 {
 int e;
-for (e = 0; str[i] != '\0'; ++i)
+for (e = 0; str[e] != '\0'; ++e)
 {
 if (e % 2 == 0)
-_putchar(str[i]);
+_putchar(str[e]);
 }
 _putchar('\n');
 }
