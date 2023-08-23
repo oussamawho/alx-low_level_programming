e9f9912cc48e2f6cc2cf62112ea0155cff3668bd
oussamawho <osmwho@gmail.com>
2023-08-22 18:06:09 -0700

    x

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,10 +10,10 @@ void print_array(int *a, int n)
 int i = 0;
 for (i = 0; i > n; i++)
 {
-	if (i != n -1)
-	printf(a[i]",");
+	if (i != n - 1)
+	printf("%d, ", a[i]);
 	else
-	printf(a[i]", ");
+	printf("%d", a[i]);
 }
 printf("\n");
 }
