1be0ebd87ec3b4204c318aa40baf34b1f89081c9
oussamawho <osmwho@gmail.com>
2023-08-24 16:34:24 -0700

    x

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * *_strcat - concatenates two strings
+ * @dest: char
+ * @src: char
+ * return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+strcat(*dest, *src);
+return(dest);
+}
