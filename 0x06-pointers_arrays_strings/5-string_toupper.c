02d74acd6023c193a761bda7239e43a8fc4b0c45
oussamawho <osmwho@gmail.com>
2023-08-25 07:35:34 -0700

    x

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * string_toupper - changes all lowercase
+ * letters of a string to uppercase
+ * @str: pointer
+ */
+char *string_toupper(char str*)
+{
+int i;
+for (i = 0; *str != '\0'; i++)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] -= 32;
+}
+return (str);
+}
