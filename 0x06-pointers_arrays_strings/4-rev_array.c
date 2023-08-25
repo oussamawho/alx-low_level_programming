5c54698a2407400557007d5bf6d75436c13d877a
oussamawho <osmwho@gmail.com>
2023-08-25 07:16:44 -0700

    x

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,16 +9,12 @@
 
 void reverse_array(int *a, int n)
 {
-int i;
-int aux[n];
-
-for (int i = 0; i < n; i++)
+	int i, j;
+for (i = 0; i < n--; i++)
 {
-aux[n - 1 - i] = a[i];
+j = a[i];
+a[i] = a[n];
+a[n] = j;
+}
 }
 
-for (int i = 0; i < n; i++)
-{
-a[i] = aux[i];
-}
-}
