0e5c3837e9be33cb28df91ab65c38f9736809fb4
oussamawho <osmwho@gmail.com>
2023-08-25 06:11:49 -0700

    x

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array
+ * @n: int
+ */
+
+void reverse_array(int *a, int n)
+{
+int i;
+int aux[n];
+
+for (int i = 0; i < n; i++)
+{
+aux[n - 1 - i] = a[i];
+}
+
+for (int i = 0; i < n; i++)
+{
+a[i] = aux[i];
+}
+}
