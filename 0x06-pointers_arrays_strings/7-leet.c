1725c7aa55daa858ffc3cf9d9fccbbc392cb8e0d
oussamawho <osmwho@gmail.com>
2023-08-25 12:22:13 -0700

    x

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,23 +1,30 @@
 #include "main.h"
 
+/**
+ * leet - encodes a string into 1337
+ * @c: pointer
+ * @alpha: array
+ * @num: array
+ * return: cp
+ */
 
 char *leet(char *c)
 {
-	char *cp = c;
-	char alph[] = {'A', 'E', 'O', 'T', 'L'};
-	int num = {4, 3, 0, 7, 1};
-	int i;
+char *cp = c;
+char alph[] = {'A', 'E', 'O', 'T', 'L'};
+int num = {4, 3, 0, 7, 1};
+int i;
 
-	while (*c)
-	{
-		for (i = 0; i < sizeof(alph) / sizeof(char); i++)
-		{
-			if (*c == alph[i] || *c == alph[i] +32)
-			{
-				*c = 48 + num[i];
-			}
-		}
-		c++;
-	}
-	return (cp);
+while (*c)
+{
+for (i = 0; i < sizeof(alph) / sizeof(char); i++)
+{
+if (*c == alph[i] || *c == alph[i] + 32)
+{
+*c = 48 + num[i];
+}
+}
+c++;
+}
+return (cp);
 }
