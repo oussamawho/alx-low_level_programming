a9cc9843c98fae83e00ea956b907ee150f5bb5ce
oussamawho <osmwho@gmail.com>
2023-08-28 16:41:47 -0700

    x

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,10 +6,12 @@
  * @n: int
  * Return: pointer s
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
+{
 unsigned int i;
 for (i = 0; n > 0; i++; n--)
 {
 s[i] = b;
 }
 return (s);
+}
