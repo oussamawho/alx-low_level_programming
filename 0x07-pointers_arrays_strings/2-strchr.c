58d8c1339af6841986f9d8da90fc92c0c29bd948
oussamawho <osmwho@gmail.com>
2023-08-28 17:39:00 -0700

    x

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**]
+ * _strchr - locates a character in a string
+ * @s: char
+ * @c:char
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+for (int i = 0; s[i] >= '\0'; i++)
+{
+if (s[i] == c)
+return (&s[i]);
+}
+return (0);
+}
