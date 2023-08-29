ddde92548aaba5f5e484fe620ae3fb3fefd8ed72
oussamawho <osmwho@gmail.com>
2023-08-28 19:00:38 -0700

    x

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strstr - locates a substring
+ * @haystack: char
+ * @needle: char
+ * Return: Always 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+while (*haystack != '\0')
+{
+haystack++;
+char *s = haystack;
+char *k = needle;
+while (*s == *k && *k != '\0')
+{
+s++;
+k++;
+}
+if (*k == '\0')
+return (haystack);
+}
+return (0);
+}
