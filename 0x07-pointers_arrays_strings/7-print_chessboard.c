4a5acab0ec349ef1aec4fb9bec939afbf242afd1
oussamawho <osmwho@gmail.com>
2023-08-29 16:57:15 -0700

    x

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - that prints the chessboard
+ * @: rows
+ * Return: 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+int l, y;
+for (l = 0; l < 0; l++)
+{
+	for (y = 0; y < 0; y++)
+	{
+		_putchar(a[l][y]);
+	}
+	_putchar('\n');
+}
+}
