7ff5ea46b5bdbc69688c321380dbf543dabd8c6e
oussamawho <osmwho@gmail.com>
2023-08-28 18:32:38 -0700

    x

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _strspn - Entry point
- * @s: input
- * @accept: input
- * Return: Always 0 (Success)
+ * _strspn -  get the length of a prefix substring
+ * @s: char
+ * @accept: input char
+ * Return: Always 0
  */
 unsigned int _strspn(char *s, char *accept)
 {
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *_strpbrk - searche a string for any of a set of bytes
+ * @s : char
+ * @accept: char 
+ * Return: pointer s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int n, l;
+char *q;
+n = 0;
+while (s[n] !=m '\0')
+{
+l = 0;
+while (accept[l] != '\0')
+{
+if (accept[l] == s[n])
+{
+q = &s[n];
+return (q);
+}
+l++;
+}
+n++;
+}
+return (0);
+}
