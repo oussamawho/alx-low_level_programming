edee9998c5964fd2f1735330acda1f682552cf9f
oussamawho <osmwho@gmail.com>
2023-09-02 17:26:10 -0700

    x

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - converts a string to an integer
+ * @argc: int
+ * @argv: char
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int sum;
+if (argc == 3)
+{
+sum = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", sum);
+}
+else
+{
+printf("error\n");
+return (1);
+
+return (0);
+}
