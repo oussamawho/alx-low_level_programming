0df80a7f021087e397457956c49dfed5bd34114f
oussamawho <osmwho@gmail.com>
2023-09-02 17:47:41 -0700

    x

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,7 +15,7 @@ while (--argc)
 for (c = argv[argc]; *c; c++)
 if (*c < '0' || *c > '9')
 return (printf("Error\n"), 1);
-sum += atoi(argv[argc];
+sum += atoi(argv[argc]);
 }
 printf("%d\n", sum);
 return (0);
