4919820e7d4dd0eca72b15520aab86745ead413a
oussamawho <osmwho@gmail.com>
2023-09-02 17:40:09 -0700

    x

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check - string there are digit
+ * @argv: array
+ * @argc: int
+ * Return:0
+ */
+int main(int argc, char *argv[])
+{
+int sum = 0;
+char *c;
+while (--argc)
+{
+for (c = argv[argc]; *c; c++)
+if (*c < '0' || *c > '9')
+return (printf("error\n"), 1);
+sum += atoi(argv[argc];
+}
+printf("%d\n", sum);
+return (0);
+}
