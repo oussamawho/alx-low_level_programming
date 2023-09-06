93f5838b1b28785fe9579efb2ac1b6df91401474
oussamawho <osmwho@gmail.com>
2023-09-05 17:37:33 -0700

    x

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,7 +10,7 @@
 int **alloc_grid(int width, int height)
 {
 int **tab, x, a;
-tab = malloc(sizeof(*tab)* height);
+tab = malloc(sizeof(*tab) * height);
 if (width <= 0 || height <= 0 || tab == 0)
 {
 return (NULL);
@@ -19,16 +19,16 @@ else
 {
 for (x = 0; x < height; x++)
 {
-tab[x] = malloc(sizeof(**tab)* width);
+tab[x] = malloc(sizeof(**tab) * width);
 if (tab[x] == 0)
 {
 /*Free everything if malloc fails*/
 while (x--)
-free (tab[x]);
-free (tab);
+free(tab[x]);
+free(tab);
 return (NULL);
 }
-for (a = 0; a< width; a++)
+for (a = 0; a < width; a++)
 tab[x][a] = 0;
 }
 }
