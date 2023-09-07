5f8e0de307fafffbb39160030f8c245a3ce5f669
oussamawho <osmwho@gmail.com>
2023-09-07 15:36:39 -0700

    x

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* malloc_checked - allocate memory using malloc
+* @b: int
+* Return: pointer to the array
+*/
+void *malloc_checked (unsigned int b)
+}
+int *x = malloc(b);
+if (x == 0)
+exit (98);
+return (m);
+}
