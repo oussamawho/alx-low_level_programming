948b1ee1976c70750c0ccdb80dd817edc091aadd
oussamawho <osmwho@gmail.com>
2023-09-15 17:47:29 -0700

    x

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,15 +11,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list arg;
-	int sum = 0;
+	va_list ap;
+	unsigned int i, sum = 0;
 
-	va_start(arg, n);
-	unsigned int i;
+	va_start(ap, n);
+
 	for (i = 0; i < n; i++)
-		sum += va_arg(arg, int);
+		sum += va_arg(ap, int);
 
-	va_end(arg);
+	va_end(ap);
 
 	return (sum);
 }
