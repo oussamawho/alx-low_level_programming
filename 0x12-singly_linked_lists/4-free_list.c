2b11392a24188e2897c21c64514dd9573dbb7686
oussamawho <osmwho@gmail.com>
2023-09-21 17:44:22 -0700

    x

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
+void free_list(list_t *head)
+{
+if (head)
+{
+free_list(head->next);
+free(head->str);
+free(head);
+}
+}
