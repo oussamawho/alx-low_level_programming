8026c29b75ebb04883f1b98858ddeff233776e31
oussamawho <osmwho@gmail.com>
2023-09-21 17:54:05 -0700

    x

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,24 +1,11 @@
 #include <stdio.h>
-void pre_main_message(void);
-/* Define a global variable to indicate whether the message has been printed */
-int message_printed = 0;
-int main(void)
+void first(void) __attribute__ ((constructor));
+/**
+ * first - prints a sentence before the main
+ * function is executed
+ */
+void first(void)
 {
-/* Call the pre_main_message function before executing main */
-pre_main_message();
-/* Rest of the main function */
-printf("This is the main function.\n");
-
-return (0);
-}
-
-/* Define a function to print the message before main */
-void pre_main_message(void)
-{
-if (!message_printed)
-{
-printf("You're beat! and yet, you must allow,\n");
-printf("I bore my house upon my back!\n");
-message_printed = 1;
-}
+ printf("You're beat! and yet, you must allow,\n");
+ printf("I bore my house upon my back!\n");
 }
