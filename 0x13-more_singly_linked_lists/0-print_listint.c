091795da6269c9fa8f7bfedaaadf8bc5f418e04e
oussamawho <osmwho@gmail.com>
2023-09-26 15:52:41 -0700

    x

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,8 +2,7 @@
 
 /**
  * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
- *
+ * @h: linked list
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
