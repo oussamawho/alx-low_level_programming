d97dc6ba5b4fad1d6871c7fa9b64e685d803044d
oussamawho <osmwho@gmail.com>
2023-09-26 17:36:04 -0700

    x

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,30 +8,33 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-size_t l = 0;
-int d;
-listint_t *temp;
+	size_t l = 0;
+	int d;
+	listint_t *temp;
 
-if (!h || !*h)
-return (0);
-while (*h)
-{
-d = *h - (*h)->next;
-if (d > 0)
-{
-temp = (*h)->next;
-free(*h);
-*h = temp;
-l++;
-}
-else
-{
-free(*h);
-*h = NULL;
-l++;
-break;
-}
-}
-*h = NULL;
-return (l);
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		d = *h - (*h)->next;
+		if (d > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			l++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			l++;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (l);
 }
