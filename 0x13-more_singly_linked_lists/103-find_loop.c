392884c4922ba7f5c89bbc2e918f447e1b91ade2
oussamawho <osmwho@gmail.com>
2023-09-26 17:28:31 -0700

    x

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -8,24 +8,24 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *s = head;
-	listint_t *f = head;
-	if (!head)
-		return (NULL);
-	while (s && f && f->next)
-	{
-		f = f->next->next;
-		s = s->next;
-		if (f == s)
-		{
-			s = head;
-			while (s != f)
-			{
-				s = s->next;
-				f = f->next;
-			}
-			return (f);
-		}
-	}
+listint_t *s = head;
+listint_t *f = head;
+if (!head)
+return (NULL);
+while (s && f && f->next)
+{
+	f = f->next->next;
+	s = s->next;
+	if (f == s)
+{
+	s = head;
+	while (s != f)
+{
+	s = s->next;
+	f = f->next;
+}
+	return (f);
+}
+}
 	return (NULL);
 }
