cd3bcea6601de230562665ea119177e77a0459d8
oussamawho <osmwho@gmail.com>
2023-09-29 10:50:19 -0700

    x

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,15 +10,13 @@
 unsigned int binary_to_uint(const char *b)
 {
 int x;
-int n;
 	if (!b)
 	return (0);
 	while (*b)
 {
 	if (*b != '0' && *b != '1')
 	return (0);
-	x = x + (*b++ * pow(2, n));
-	n++;
+	x = x * 2 + (*b++ - '0');
 }
 	return (x);
 }
