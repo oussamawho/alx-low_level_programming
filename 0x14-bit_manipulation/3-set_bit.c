f91256ddfd036398f08b8fb6c1d68c3a0a7b3600
oussamawho <osmwho@gmail.com>
2023-09-29 16:39:22 -0700

    x

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -8,15 +7,10 @@
  *
  * Return: 1 if it worked, or -1 if an error occurred.
  */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
-if (index >= sizeof(unsigned long int) * 8)
-{
+if (index >= sizeof(n) * 8)
 return (-1);
-}
-
-unsigned long int mask = 1 << index;
-*n = *n | mask;
-
-return (1);
+return (!!(*n |= 1L << index));
 }
