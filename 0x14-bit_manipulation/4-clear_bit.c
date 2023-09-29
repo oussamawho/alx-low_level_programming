f937f87e102222939075c6b7f5947507104f69e5
oussamawho <osmwho@gmail.com>
2023-09-29 16:45:44 -0700

    x

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,12 +9,9 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-if (index >= sizeof(unsigned long int) * 8)
-{
+if (index >= sizeof(n) * 8) 
 return (-1);
-}
-
-unsigned long int mask = 1 << index;
-*n = *n & ~mask;
+if (*n & 1L << index)
+*n ^= 1L << index;
 return (1);
 }
