fcbfd5b8f502debb2f87138a28af116ec3e1be9f
oussamawho <osmwho@gmail.com>
2023-09-29 16:29:38 -0700

    x

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,21 +7,11 @@
  * @index: The index of the bit to retrieve (starting from 0).
  *
  * Return: The value of the bit at the specified index
- * or -1 if an error occurred.
+ * or -1 if an error occurr
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-if (index >= sizeof(unsigned long int) * 8)
-{
+if (index >= sizeof(n) * 8)
 return (-1);
-}
-unsigned long int mask = 1 << index;
-if ((n & mask) == 0)
-{
-return (0);
-}
-else
-{
-return (1);
-}
+return (n >> index & 1);
 }
