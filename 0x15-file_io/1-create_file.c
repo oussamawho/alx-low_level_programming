b21391af4acd9e6ff8c707b22259ce5460c8c25e
oussamawho <osmwho@gmail.com>
2023-10-06 16:58:43 -0700

    x

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,25 +9,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd, writ;
-
+{
+int fd, writ, len = 0;
 if (filename == NULL)
-return (-1);
-
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-if (fd == -1)
 return (-1);
 
 if (text_content != NULL)
 {
-writ = write(fd, text_content, strlen(text_content));
-if (writ == -1)
-{
-close(fd);
+for (len = 0; text_content[len];)
+len++;
+}
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+writ = write(fd, text_content, len);
+
+if (fd == -1 || w == -1)
 return (-1);
-}
-}
 
 close(fd);
 return (1);
 }
+
