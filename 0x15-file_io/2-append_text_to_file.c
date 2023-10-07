bb1e5b6a21cdaf1e845257315ee7efb0b1a061a4
oussamawho <osmwho@gmail.com>
2023-10-06 17:38:39 -0700

    x

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,22 +11,28 @@
  */
 
 int append_text_to_file(const char *filename, char *text_content)
-{
-if (filename == NULL)
-{
-return (-1);
-}
 
-FILE *file = fopen(filename, "a");
-if (file == NULL)
 {
-return (-1);
-}
+	int len = 0;
 
-if (text_content != NULL)
-{
-fputs(text_content, file);
-}
-fclose(file);
-return (1);
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	int writ;
+	int x;
+
+	x = open(filename, O_WRONLY | O_APPEND);
+	writ = write(x, text_content, len);
+
+	if (x == -1 || writ == -1)
+		return (-1);
+
+	close(x);
+
+	return (1);
 }
