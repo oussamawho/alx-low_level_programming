092feeb48ebfe67e46a9b7a550faa3b5e159af5e
oussamawho <osmwho@gmail.com>
2023-10-06 17:23:45 -0700

    x

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -42,8 +42,10 @@ header->e_ident[EI_MAG0],
 header->e_ident[EI_MAG1],
 header->e_ident[EI_MAG2],
 header->e_ident[EI_MAG3]);
-printf(" Class: %s\n", header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
-printf(" Data: %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+printf(" Class: %s\n", header->e_ident[EI_CLASS] == ELFCLASS64 ?
+	"ELF64" : "ELF32");
+printf(" Data: %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ?
+	"2's complement, little endian" : "2's complement, big endian");
 printf(" Version: %d (current)\n", header->e_ident[EI_VERSION]);
 printf(" OS/ABI: %s\n",
 header->e_ident[EI_OSABI] == ELFOSABI_NONE ? "UNIX - System V" :
